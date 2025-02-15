7795aea76207c00e6e53fbeb5433498702648573
prakash9724 <hellobrother91069@gmail.com>
2025-02-15 16:52:46 +0530

    added animation to hero sections video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,8 @@
 import React, { useRef, useState } from "react";
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
   const [currentIndex, setCurrentIndex] = useState(1);
@@ -8,6 +12,32 @@ const Hero = () => {
 
   const totalVideos = 4;
   const nextVideoRef = useRef(null);
+
+  useGSAP(()=>{
+    if(hasClicked){
+      gsap.set('#next-video',{visibility:'visible'});
+      gsap.to('#next-video',{
+        transformOrigin:'center center',
+        scale:1,
+        width:'100%',
+        height:'100%',
+        duration:1,
+        ease:'power1.inOut',
+        onStart:()=> nextVideoRef.current.play(),
+      })
+
+      gsap.from('#current-video',{
+        transformOrigin:'center center',
+        scale:0,
+        duration:1.5,
+        ease:'power1.inOut'
+      })
+    }
+  },{dependencies:[currentIndex],revertOnUpdate:true})
+
+
+
+  
 
   const upcomingVideoIndex = (currentIndex % totalVideos) + 1;
 
@@ -72,9 +102,12 @@ const Hero = () => {
             <div className="mt-24 px-5 sm:px-10">
                 <h1 className="special-font hero-heading text-blue-100">redefi<b>n</b>e</h1>
                 <p className="mb-5 max-w-64 font-robert-regular text-blue-100">Enter the Metagame Layer <br /> Unleash the play Economy</p>
+                <Button id="watch-trailer" title="Watch Trailer" leftIcon={<TiLocationArrow/>} containerClass="!bg-yellow-300 flex-center gap-1"/>
             </div>
         </div>
       </div>
+      <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">G<b>a</b>ming</h1>
+
     </div>
   );
 };
